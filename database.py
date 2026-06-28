cbdf9f4b2aaa8dab0148ca36e45341bec880aef8
pacheco-p <pedroprince53@gmail.com>
2026-06-28 15:33:04 +0100

    database.py

--- database.py
+++ database.py
@@ -1,9 +1,13 @@
 import sqlite3
 
 conn = sqlite3.connect("cgpa.db", check_same_thread=False)
+conn.row_factory = sqlite3.Row
 cursor = conn.cursor()
 
-# Create Users Table
+# ==========================
+# USERS TABLE
+# ==========================
+
 cursor.execute("""
 CREATE TABLE IF NOT EXISTS users(
     id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -19,7 +23,10 @@ CREATE TABLE IF NOT EXISTS users(
 )
 """)
 
-# Create Results Table
+# ==========================
+# RESULTS TABLE
+# ==========================
+
 cursor.execute("""
 CREATE TABLE IF NOT EXISTS results(
     id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -34,6 +41,10 @@ CREATE TABLE IF NOT EXISTS results(
 
 conn.commit()
 
+
+# ==========================
+# USER FUNCTIONS
+# ==========================
 
 def create_user(username, email, password):
     try:
@@ -55,57 +66,17 @@ def get_user(username):
     return cursor.fetchone()
 
 
-def update_profile(
-    username,
-    full_name,
-    matric_number,
-    department,
-    faculty,
-    level,
-    admission_year
-):
+def update_profile(username, full_name, matric_number,
+                   department, faculty, level, admission_year):
+
     cursor.execute("""
-        UPDATE users
-        SET
-            full_name=?,
-            matric_number=?,
-            department=?,
-            faculty=?,
-            level=?,
-            admission_year=?
-        WHERE username=?
-    """, (
-        full_name,
-        matric_number,
-        department,
-        faculty,
-        level,
-        admission_year,
-        username
-    ))
-    conn.commit()
-
-
-def get_profile(username):
-    cursor.execute(
-        "SELECT * FROM users WHERE username=?",
-        (username,)
-    )
-    return cursor.fetchone()
-
-
-def save_result(username, session, semester, gpa, cgpa):
-    cursor.execute("""
-        INSERT INTO results(username, session, semester, gpa, cgpa)
-        VALUES (?, ?, ?, ?, ?)
-    """, (username, session, semester, gpa, cgpa))
-    conn.commit()
-
-
-def get_results(username):
-    cursor.execute("""
-        SELECT * FROM results
-        WHERE username=?
-        ORDER BY id DESC
-    """, (username,))
-    return cursor.fetchall()
+    UPDATE users
+    SET
+        full_name=?,
+        matric_number=?,
+        department=?,
+        faculty=?,
+        level=?,
+        admission_year=?
+    WHERE username=?
+    """,(
