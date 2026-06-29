814100f0eb6387d74c4e2603874c5ba899884236
pacheco-p <pedroprince53@gmail.com>
2026-06-29 15:37:20 +0100

    Update settings.py

--- settings.py
+++ settings.py
@@ -1,13 +1,10 @@
 import streamlit as st
 
 def show():
-
     st.title("⚙️ Settings")
 
-    dark = st.toggle("Dark Mode")
+    dark = st.toggle("Dark Theme Mode")
+    notify = st.toggle("Enable Calculation Auto-backups")
 
-    notify = st.toggle("Enable Notifications")
-
-    if st.button("Save Settings"):
-
-        st.success("Settings Saved")
+    if st.button("Save Settings Configuration", use_container_width=True):
+        st.success("Configuration preferences updated and saved successfully.")
